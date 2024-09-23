575210bd8d089443e2f11ce52754fa31bd72ccd6
Leobardo Luis Argüelles <66451459+LeobardoArguelles@users.noreply.github.com>
2024-09-23 12:09:06 -0700

    Customize instead of create

---
+++ template/src/lib/get-dictionary.ts
@@ -0,0 +1,14 @@
+import type { Locale } from "@/i18n-config";
+
+const dictionaries = {
+  en: () =>
+    import("../dictionaries/main/en.json").then((module) => module.default),
+  es: () =>
+    import("../dictionaries/main/es.json").then((module) => module.default),
+};
+
+export const getDictionary = async (locale: Locale) => {
+  console.log("At get-dictionary.ts");
+  console.log("locale: ", locale);
+  return dictionaries[locale];
+};
